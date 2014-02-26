a0162590c6b9ecc243856ea0570ef978adfc49cd
randompherret <derek@randompherret.com>
2014-02-25 21:25:13 -0600

    config and setup databases
    make sure to change salt and seed in production

--- api/app/Config/core.php
+++ api/app/Config/core.php
@@ -222,12 +222,12 @@
 /**
  * A random string used in security hashing methods.
  */
-	Configure::write('Security.salt', 'DYhG93b0qyJfIxfs2guVoUubWwvniR2G0FgaC9mi');
+	Configure::write('Security.salt', 'wdhiq3jrio3tjfoid09240923iodkpoackvybunimo09309123');
 
 /**
  * A random numeric string (digits only) used to encrypt/decrypt strings.
  */
-	Configure::write('Security.cipherSeed', '76859309657453542496749683645');
+	Configure::write('Security.cipherSeed', '567890454618465498165499191263238183918741');
 
 /**
  * Apply timestamps with the last modified time to static assets (js, css, images).
